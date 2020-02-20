eba928982e275a3314bc4bce0500ccbcdfb2ca3d
creatorscan <creatorscan@gmail.com>
2020-02-20 20:02:01 +0100

    2nd commit

---
+++ ci/test_integration.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# test asr recipe
+(
+    cd ./egs/mini_an4/asr1 || exit 1
+    . path.sh  # source here to avoid undefined variable errors
+
+    set -euo pipefail
+
+    echo "==== ASR (backend=pytorch) ==="
+    ./run.sh
+    echo "==== ASR (backend=pytorch, dtype=float64) ==="
+    ./run.sh --stage 3 --train-config "$(change_yaml.py conf/train.yaml -a train-dtype=float64)" --decode-config "$(change_yaml.py conf/decode.yaml -a api=v2 -a dtype=float64)"
+    echo "==== ASR (backend=chainer) ==="
+    ./run.sh --stage 3 --backend chainer
+)
+# test asr_mix recipe
+(
+    set -euo pipefail
+
+    cd ./egs/mini_an4/asr_mix1 || exit 1
+    echo "==== ASR Mix (backend=pytorch) ==="
+    ./run.sh
+)
+# test tts recipe
+(
+    set -euo pipefail
+
+    cd ./egs/mini_an4/tts1 || exit 1
+    echo "==== TTS (backend=pytorch) ==="
+    ./run.sh
+)
+
+# TODO(karita): test mt, st?
